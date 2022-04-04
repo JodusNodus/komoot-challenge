6cb1e962d92122dd75e80f9c4545c30cd0334b9e
Thomas Billiet <thomas.billiet@spencer.co>
2022-04-04 11:23:30 +0100

    move code

--- src/Map/TileLayer.tsx
+++ src/Map/TileLayer.tsx
@@ -10,29 +10,30 @@ type TProps = {
 };
 
 export function TileLayer({ viewport }: TProps) {
-  const mapTilesFit = Math.floor(viewport.mapSize * viewport.scale) / TILE_SIZE;
-  const zoom = bound(Math.ceil(Math.log2(mapTilesFit - 3)), 4, 18);
-
-  const divider = Math.pow(2, zoom);
-  const size = viewport.mapSize / divider;
-
-  const startTileX = Math.floor(viewport.x / size);
-  const endTileX = bound(
-    Math.ceil((viewport.x + viewport.width) / size),
-    0,
-    divider
-  );
-  const startTileY = Math.floor(viewport.y / size);
-  const endTileY = bound(
-    Math.ceil((viewport.y + viewport.height) / size),
-    0,
-    divider
-  );
-
-  const horizontalTiles = endTileX - startTileX;
-  const verticalTiles = endTileY - startTileY;
-
   const tiles = React.useMemo(() => {
+    const mapTilesFit =
+      Math.floor(viewport.mapSize * viewport.scale) / TILE_SIZE;
+    const zoom = bound(Math.ceil(Math.log2(mapTilesFit - 3)), 4, 18);
+
+    const divider = Math.pow(2, zoom);
+    const size = viewport.mapSize / divider;
+
+    const startTileX = Math.floor(viewport.x / size);
+    const endTileX = bound(
+      Math.ceil((viewport.x + viewport.width) / size),
+      0,
+      divider
+    );
+    const startTileY = Math.floor(viewport.y / size);
+    const endTileY = bound(
+      Math.ceil((viewport.y + viewport.height) / size),
+      0,
+      divider
+    );
+
+    const horizontalTiles = endTileX - startTileX;
+    const verticalTiles = endTileY - startTileY;
+
     if (!(horizontalTiles * verticalTiles)) return [];
     return new Array(horizontalTiles * verticalTiles).fill(0).map((_, i) => {
       const x = startTileX + (i % horizontalTiles);
